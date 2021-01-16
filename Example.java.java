17f9530429f4bb02ec57aab09019338b3bd1a3ec
shagun16 <952037+shagun16@users.yuhuu.github.com>
2021-01-16 10:22:12 +0530

    intro programs

---
+++ Example.java.java
@@ -0,0 +1,10 @@
+class Example {
+public static void main(String[] args){
+int num;
+num=100;
+system.out.print("this is num: " + num);
+num=num*2;
+system.out.print("the value of num * 2 is ");
+system.out.print(num);
+}
+}
---
+++ Example2.java
@@ -0,0 +1,11 @@
+class Akshu{
+public static void main (string args[])
+{
+int a,b,c;
+a=1;
+b=2;
+c=a+b;
+{
+system.out.print(c);
+}
+}
